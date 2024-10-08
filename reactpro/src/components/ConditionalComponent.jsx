a47663d6b6db094d2d607a6e7be9960d811259d4
DarwinMelaya <116789003+DarwinMelaya@users.noreply.github.com>
2024-10-09 01:33:48 +0800

    USING TERNARY OPERATORS IN REAC

--- reactpro/src/components/ConditionalComponent.jsx
+++ reactpro/src/components/ConditionalComponent.jsx
@@ -2,14 +2,9 @@ import Code from "./Code";
 import Welcome from "./Welcome";
 
 const ConditionalComponent = () => {
-  let message;
   const display = true;
-  if (display) {
-    message = <h1>This is Message 1</h1>;
-  } else {
-    message = <h1>This is Message 2</h1>;
-  }
-  return message;
+
+  return display ? <Welcome /> : <Code />;
 };
 
 export default ConditionalComponent;
