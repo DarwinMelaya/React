9987b11daf116d4e522a874742bfe689eeb20ae4
DarwinMelaya <116789003+DarwinMelaya@users.noreply.github.com>
2024-10-09 10:08:30 +0800

    EVENT HANDLING IN REACT

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -1,10 +1,11 @@
 import ConditionalComponent from "./components/ConditionalComponent";
 import Fruits from "./components/Fruits";
+import Message from "./components/Message";
 
 function App() {
   return (
     <div className="App">
-      <Fruits />
+      <Message />
     </div>
   );
 }
